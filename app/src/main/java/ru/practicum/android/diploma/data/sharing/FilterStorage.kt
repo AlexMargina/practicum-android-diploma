ae2ead01492243ae72609640e3f9110131c5ce38
Git_maalmi <marginali@ya.ru>
2024-02-04 01:10:25 +0300

    Создал FiltersDto и SharedPrefStorage

---
+++ app/src/main/java/ru/practicum/android/diploma/data/sharing/FilterStorage.kt
@@ -0,0 +1,9 @@
+package ru.practicum.android.diploma.data.sharing
+
+import ru.practicum.android.diploma.data.dto.FiltersDto
+
+
+interface FiltersStorage {
+    fun doRequest(): FiltersDto
+    fun doWrite(filtersDto: FiltersDto)
+}
