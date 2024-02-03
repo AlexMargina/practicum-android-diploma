89595b4dfaeb15c4d5605990f2225665cc2a1f15
Bugrov1 <px316@yandex.ru>
2024-02-03 23:47:54 +0300

    Фильтр и Репозиторий

---
+++ app/src/main/java/ru/practicum/android/diploma/data/repository/FilterRepositoryImpl.kt
@@ -0,0 +1,23 @@
+package ru.practicum.android.diploma.data.repository
+
+import android.content.SharedPreferences
+import com.google.gson.Gson
+import ru.practicum.android.diploma.domain.models.Filter
+import ru.practicum.android.diploma.domain.api.FilterRepository
+
+const val FILTER_STORAGE = "filter_storage"
+const val FILTER_KEY = "filter_key"
+
+class FilterRepositoryImpl(val sharedPreferences: SharedPreferences) : FilterRepository {
+    override fun load(): Filter? {
+        val json = sharedPreferences.getString(FILTER_KEY, null)
+        return Gson().fromJson(json, Filter::class.java)
+    }
+
+    override fun write(filter: Filter) {
+        val json = Gson().toJson(filter)
+        sharedPreferences.edit()
+            .putString(FILTER_KEY, json)
+            .apply()
+    }
+}
--- app/src/main/java/ru/practicum/android/diploma/di/FilterModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/FilterModule.kt
@@ -1,11 +1,19 @@
 package ru.practicum.android.diploma.di
 
+import android.app.Application
+import android.content.Context.MODE_PRIVATE
+import android.content.SharedPreferences
 import org.koin.dsl.module
 import ru.practicum.android.diploma.data.conventers.IndustryMapper
+import ru.practicum.android.diploma.data.repository.FILTER_STORAGE
+import ru.practicum.android.diploma.data.repository.FilterRepositoryImpl
 import ru.practicum.android.diploma.data.repository.IndustryRepositoryImpl
+import ru.practicum.android.diploma.domain.api.FilterInteractor
+import ru.practicum.android.diploma.domain.api.FilterRepository
 import ru.practicum.android.diploma.domain.api.IndustryInteractor
 import ru.practicum.android.diploma.domain.api.IndustryRepository
 import ru.practicum.android.diploma.domain.impl.IndustryInteractorImpl
+import ru.practicum.android.diploma.domain.repository.FilterInteractorImpl
 
 val filterModule = module {
 
@@ -18,4 +26,20 @@ val filterModule = module {
         IndustryRepositoryImpl(networkClient = get(), context = get(), mapper = get())
     }
     factory { IndustryMapper() }
+
+    single<SharedPreferences>() {
+        val application = get<Application>()
+        application.getSharedPreferences(
+            FILTER_STORAGE,
+            MODE_PRIVATE
+        )
+    }
+
+    single<FilterRepository> {
+        FilterRepositoryImpl(sharedPreferences = get())
+    }
+
+    single<FilterInteractor> {
+        FilterInteractorImpl(filterRepository = get())
+    }
 }
