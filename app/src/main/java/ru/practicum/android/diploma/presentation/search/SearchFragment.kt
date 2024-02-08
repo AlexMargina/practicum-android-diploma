74c788d94dc932c6cc6c1810bd6a03fa048dec17
ZaharinVN <vitalikz85@yandex.ru>
2024-02-09 00:22:07 +0300

    Меняет иконку перехода к фильтрам на экране поиска при непустом значении фильтров

--- app/src/main/java/ru/practicum/android/diploma/presentation/search/SearchFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/presentation/search/SearchFragment.kt
@@ -15,6 +15,8 @@ import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Observer
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -46,6 +48,7 @@ class SearchFragment : Fragment() {
     private var isNeedAddItems = true
     private var lastSearchText = ""
     private var newSearchText = ""
+    private val sharedViewModel: SharedViewModel by activityViewModels()
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -71,7 +74,13 @@ class SearchFragment : Fragment() {
 
         initRecyclerView()
         initClickListener()
-
+        sharedViewModel.isFilterOn.observe(viewLifecycleOwner, Observer { isFilterOn ->
+            if (isFilterOn) {
+                binding.filterButton.setImageResource(R.drawable.filter_on)
+            } else {
+                binding.filterButton.setImageResource(R.drawable.ic_filter_off)
+            }
+        })
     }
 
     private fun initObservers() {
@@ -252,6 +261,7 @@ class SearchFragment : Fragment() {
             findNavController().navigate(R.id.action_searchFragment_to_filtersSettingsFragment)
         }
     }
+
 
     private fun hideKeyBoard() {
         val inputMethodManager =
